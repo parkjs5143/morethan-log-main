6b533d20c8c14a7bf80e260e1a63378168c1dbad
parkjs5143 <parkjs5143@gmail.com>
2024-01-07 14:13:01 +0900

    init

--- site.config.js
+++ site.config.js
@@ -1,29 +1,29 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "soonipy",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
+    bio: "soonipy",
     email: "morethanmin.dev@gmail.com",
     linkedin: "morethanmin",
-    github: "morethanmin",
+    github: "soonipy",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `soonipy-log`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "soonipy-log",
     description: "welcome to morethan-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://soonipy.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
@@ -55,7 +55,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "parkjs5143/blog-comments",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
